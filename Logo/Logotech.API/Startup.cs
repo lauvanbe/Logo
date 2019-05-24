546d77c5e7434552df3beee36bd012f38d09de94
lvbcpa6 <laurent.vanbeveren@colruytgroup.com>
2019-05-24 14:23:04 +0200

    connectionStringMySql

--- Logo/Logotech.API/Startup.cs
+++ Logo/Logotech.API/Startup.cs
@@ -35,7 +35,8 @@ namespace Logotech.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            // services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddJsonOptions(opt => {
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
